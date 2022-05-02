31cf12752d1899c0304cf2ce2e2c428aba95bd35
Roberto Prado <robertopfigue@gmail.com>
2022-05-02 14:58:36 -0300

    migration config

---
+++ CurriculoGerenciator.Infra/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using CurriculoGerenciator.Infra.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace CurriculoGerenciator.Infra
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CurriculoContext>
+    {
+        public CurriculoContext CreateDbContext(string[] args)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCPRE_ENVIROMENT");
+            var fileName = Directory.GetCurrentDirectory() + $"/../CurriculoGerenciator.Interface/appsettings.{environmentName}.json";
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile(fileName)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<CurriculoContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+
+            return new CurriculoContext(builder.Options);
+        }
+    }
+}
--- CurriculoGerenciator.Interface/Program.cs
+++ CurriculoGerenciator.Interface/Program.cs
@@ -6,9 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<CurriculoContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
